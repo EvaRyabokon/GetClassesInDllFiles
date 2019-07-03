59fe49151176c02d323a0e538187e7e68ff0552a
EvaRyabokon <ryabokonevgeniiya@gmail.com>
2019-07-03 19:03:41 +0500

    Implemented getting classes from DLL

---
+++ TestTask/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System.IO;
+using System.Reflection;
+
+namespace TestTask
+{
+    class Program
+    {
+        public struct ClassInfo
+        {
+            public string Name;
+            public List<string> PublicMethods;
+            public List<string> ProtectedMethods;
+
+            public ClassInfo(string name, List<string>  publicMethods, List<string> protectedMethods)
+            {
+                Name = name;
+                PublicMethods = publicMethods;
+                ProtectedMethods = protectedMethods;
+            }
+        }
+        public struct DllsInfo
+        {
+            public List<ClassInfo> Classes;
+            public List<string> FailedDlls;
+
+            public DllsInfo( List<ClassInfo> classes, List<string> failedDlls)
+            {
+                Classes = classes;
+                FailedDlls = failedDlls;
+            }
+        }
+
+        public static ClassInfo GetMethodsInType(Type type)
+        {
+            List<string> publicMethods = new List<string>();
+            List<string> protectedMethods = new List<string>();
+
+            foreach (MethodInfo m in type.GetMethods(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                if (m.IsPublic)
+                {
+                    publicMethods.Add(m.Name);
+                }
+
+                if (m.IsFamily)
+                {
+                    protectedMethods.Add(m.Name);
+                }
+            }
+
+            return new ClassInfo(type.Name, publicMethods, protectedMethods);
+        }
+
+        public static List<ClassInfo> GetClassesInFile(string path)
+        {
+            try
+            {
+                List<ClassInfo> classes = new List<ClassInfo>();
+
+                Assembly a = Assembly.LoadFrom(path);
+                Type[] types = a.GetTypes();
+                foreach (Type type in types)
+                {
+                    classes.Add(GetMethodsInType(type));
+                }
+                return classes;
+            }
+
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static DllsInfo GetMethodsFromDllFiles(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
+
+            List<ClassInfo> classesInfo = new List<ClassInfo>();
+            List<string> failedDlls = new List<string>();
+
+            foreach (var file in filePaths)
+            {
+                List<ClassInfo> classes = GetClassesInFile(file);
+
+                if (classes == null)
+                {
+                    failedDlls.Add(file);
+                }
+                else
+                {
+                    foreach (var c in classes)
+                    {
+                        classesInfo.Add(c);
+                    }
+                }
+            }
+
+            return new DllsInfo(classesInfo, failedDlls);
+        }
+
+        public static void PrintResult(DllsInfo files)
+        {
+            foreach (var c in files.Classes)
+            {
+                Console.WriteLine("CLASS: " + c.Name);
+                foreach (var protectedMethod in c.ProtectedMethods)
+                {
+                    Console.WriteLine("PROTECTED: " + protectedMethod);
+                }
+
+                foreach (var publicMethod in c.PublicMethods)
+                {
+                    Console.WriteLine("PUBLIC: " + publicMethod);
+                }
+            }
+
+            foreach (var dll in files.FailedDlls)
+            {
+                Console.WriteLine("Ошибка в формате файла");
+                Console.WriteLine(dll);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the file directory ");
+            string path = Console.ReadLine();
+            DllsInfo files = GetMethodsFromDllFiles(path);
+            PrintResult(files);
+        }
+    }
+}
